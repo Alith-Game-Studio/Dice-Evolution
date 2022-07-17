6f458755f80e46083d039d0493ab1cd6242dd91f
Daniel Chin <daniel.chin@nyu.edu>
2022-07-17 15:30:37 +0400

    red icons

--- build.py
+++ build.py
@@ -2,6 +2,9 @@ import os
 from PIL import Image
 
 GRAY = 'gray'
+RED = 'red'
+
+COLORS = [GRAY, RED]
 
 def main():
     os.chdir('texture')
@@ -9,20 +12,33 @@ def main():
     inputs = []
     for filename in filenames:
         filename = filename.lower()
-        if filename.endswith('.png') and GRAY not in filename:
-            inputs.append(filename)
+        if filename.endswith('.png'):
+            for color in COLORS:
+                if color in filename:
+                    break
+            else:
+                inputs.append(filename)
     for filename in inputs:
-        img = Image.open(filename).convert('RGBA')
-        pixels = img.load()
-        for x in range(img.width):
-            for y in range(img.height):
-                r, g, b, a = pixels[x, y]
-                pixels[x, y] = (
-                    r, 
-                    g, 
-                    b, 
-					round(a * .4)
-                )
-        img.save(filename[:-4] + '_gray.png')
+        for color in COLORS:
+            img = Image.open(filename).convert('RGBA')
+            pixels = img.load()
+            for x in range(img.width):
+                for y in range(img.height):
+                    r, g, b, a = pixels[x, y]
+                    if color == GRAY:
+                        pixels[x, y] = (
+                            r, 
+                            g, 
+                            b, 
+                            round(a * .4), 
+                        )
+                    elif color == RED:
+                        pixels[x, y] = (
+                            r, 
+                            0, 
+                            0, 
+                            255, 
+                        )
+            img.save(filename[:-4] + f'_{color}.png')
 
 main()
