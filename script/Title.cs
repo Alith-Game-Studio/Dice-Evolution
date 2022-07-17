8f957d060d5315684adf0e9e80c7b3a552f5f421
instr3 <instr3@163.com>
2022-07-17 13:58:33 +0400

    Sound

--- script/Title.cs
+++ script/Title.cs
@@ -4,11 +4,14 @@ using System;
 public class Title : Node2D {
     void OnButtonStartPressed() {
         GetTree().ChangeScene("res://Main.tscn");
+        GetNode<AudioStreamPlayer>("/root/ClickPlayer").Play();
     }
     void OnButtonCreditsPressed() {
         GetTree().ChangeScene("res://Credits.tscn");
+        GetNode<AudioStreamPlayer>("/root/ClickPlayer").Play();
     }
     void OnButtonExitPressed() {
         GetTree().Quit();
+        GetNode<AudioStreamPlayer>("/root/ClickPlayer").Play();
     }
 }
