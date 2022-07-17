742d24d5561f49cb9a3bec8057471de917f9bdb9
Daniel Chin <daniel.chin@nyu.edu>
2022-07-17 16:32:21 +0400

    merge new balance

--- script/GameState.cs
+++ script/GameState.cs
@@ -11,7 +11,7 @@ public class GameState : Node
     public static void Reset() {
 
         Inventory = new Dictionary<string, int>() {
-            {"mana", 10},
+            {"mana", 15},
             {"hp", 5 },
             {"fire", 2 },
         };
@@ -24,11 +24,11 @@ public class GameState : Node
             new DiceFacetConvert(new string[] {"fire"}, new string[]{"air"}, null, "fight"),
         });
         Dice dicePlay = new Dice("play", new DiceFacet[] {
-            new DiceFacetConvert(new string[] { }, new string[] {"mana"}, null, "play"),
-            new DiceFacetConvert(new string[] { }, new string[] {"mana"}, null, "play"),
             new DiceFacetConvert(new string[] { }, new string[] {"mana$2"}, null, "play"),
-            new DiceFacetConvert(new string[] { }, new string[] {"mana"}, null, "play"),
-            new DiceFacetConvert(new string[] { }, new string[] {"mana"}, null, "play"),
+            new DiceFacetConvert(new string[] { }, new string[] {"mana$2"}, null, "play"),
+            new DiceFacetConvert(new string[] { }, new string[] {"mana$4"}, null, "play"),
+            new DiceFacetConvert(new string[] { }, new string[] {"mana$2"}, null, "play"),
+            new DiceFacetConvert(new string[] { }, new string[] {"mana$2"}, null, "play"),
             new DiceFacetConvert(new string[] { }, new string[] {"hp"}, null, "play"),
         });
         Dice diceWork = new Dice("work", new DiceFacet[] {
@@ -40,12 +40,12 @@ public class GameState : Node
             new DiceFacetConvert(new string[] {"mana"}, new string[]{"air"}, null, "work"),
         });
         Dice diceRoot = new Dice("root", new DiceFacet[] {
-            new DiceFacetCall(new string[] { }, "work", null, "root"),
-            new DiceFacetCall(new string[] { }, "play", null, "root"),
-            new DiceFacetCall(new string[] { }, "fight", null, "root"),
-            new DiceFacetCall(new string[] { }, "work", null, "root"),
-            new DiceFacetCall(new string[] { }, "play", null, "root"),
-            new DiceFacetCall(new string[] { }, "fight", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "work", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "play", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "fight", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "work", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "play", null, "root"),
+            new DiceFacetCall(new string[] {"mana"}, "fight", null, "root"),
         });
         diceRoot.IsRevealed = true;
         Dices = new Dice[] {
